3f1ddae96eace625f1697cb2c6fc2dc24254edac
clare <clarepritchard@hotmail.com>
2022-04-26 11:14:45 +0100

    added service package and repo package, with ThingService class and ThingRepo interface

---
+++ src/main/java/com/qa/main/service/ThingService.java
@@ -0,0 +1,12 @@
+package com.qa.main.service;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.main.domain.Thing;
+
+import com.qa.main.repo.ThingRepo;
+
+@Service
+public class ThingService {
+
+}
