2b1d65f1fd390c5ff94f55a53b11529673e2d4c8
clare <clarepritchard@hotmail.com>
2022-04-26 10:34:32 +0100

    extra column and getters and setters

--- src/main/java/com/qa/main/domain/Thing.java
+++ src/main/java/com/qa/main/domain/Thing.java
@@ -22,24 +22,64 @@ public class Thing {
 	@Column
 	private String description;
 	
+	@Column 
+	private String priority;
+	
 	//constructors
 	
 	public Thing() {
 		super();
 	}
 
-	public Thing(String name, String description) {
+	public Thing(String name, String description, String priority) {
 		super();
 		this.name = name;
 		this.description = description;
+		this.priority = priority;
 	}
 
-	public Thing(long id, String name, String description) {
+	public Thing(long id, String name, String description, String priority) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
+		this.priority = priority;
 	}
+	
+	//getters and setters
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	
 	
 	
 	
