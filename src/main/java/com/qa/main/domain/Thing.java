7b44cb9dc0df58ce48fd3f8452f197b2c18040d0
clare <clarepritchard@hotmail.com>
2022-04-26 10:38:41 +0100

    to string, equals and hashcode methods added

--- src/main/java/com/qa/main/domain/Thing.java
+++ src/main/java/com/qa/main/domain/Thing.java
@@ -1,5 +1,7 @@
 package com.qa.main.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -78,6 +80,29 @@ public class Thing {
 
 	public void setPriority(String priority) {
 		this.priority = priority;
+	}
+
+	@Override
+	public String toString() {
+		return "Thing [id=" + id + ", name=" + name + ", description=" + description + ", priority=" + priority + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name, priority);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Thing other = (Thing) obj;
+		return Objects.equals(description, other.description) && id == other.id && Objects.equals(name, other.name)
+				&& Objects.equals(priority, other.priority);
 	}
 	
 	
